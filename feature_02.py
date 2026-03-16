26b6ceab6c32513967eb179f579fec3492c93d5d
Philippe Verdeja <philippeverdeja@gmail.com>
2026-03-16 21:50:26 +0100

    change

--- feature_02.py
+++
@@ -1,7 +0,0 @@
-import streamlit as st
-
-from feature_01 import printing
-
-x = printing
-
-st.write (x)
---
+++ test.py
@@ -0,0 +1,7 @@
+import streamlit as st
+
+from feature_01 import printing
+
+x = printing
+
+st.write (x)
