7208a98f6d95fccad16916228afee6be7fcfdf00
Philippe Verdeja <philippeverdeja@gmail.com>
2026-03-17 22:48:00 +0100

    comments

--- CS_PROJECT_2.py
+++ CS_PROJECT_2.py
@@ -3,30 +3,53 @@
 import streamlit as st
 from yahooquery import Ticker
 
+# title of the website
 st.title("Stock Comparator - CS_GRPOUP_PROJECT_2026_11.5")
 
+
+#text inputs for tickers
 ticker1 = st.text_input("First stock (e.g. AAPL)")
 ticker2 = st.text_input("Second stock (e.g. MSFT)")
 
+
+#from chatGPT cache function --> saves results for 10 min (600 sec), if not 
+#every refresh is a new call
 @st.cache_data(ttl=600)
+
+#definition of function (Chat GPT)
 def get_stock_info(ticker):
+
+    #creates yahoo finance object for this stock
     t = Ticker(ticker)
 
-    financial_data = t.financial_data.get(ticker.upper(), {})
-    key_stats = t.key_stats.get(ticker.upper(), {})
-    summary_detail = t.summary_detail.get(ticker.upper(), {})
+    #ticker.upper --> every input is upper case letters
+    financial_data = t.financial_data.get(ticker.upper(), {}) #probaility, margins
 
+
+    key_stats = t.key_stats.get(ticker.upper(), {}) # ratios evaluations
+
+
+    summary_detail = t.summary_detail.get(ticker.upper(), {}) #market data
+
+
+        #combines dictionaries into one (Chat gpt)
     return {**financial_data, **key_stats, **summary_detail}
 
+
+#only run program if both inputs exist
 if ticker1 and ticker2:
     stock1 = get_stock_info(ticker1)
     stock2 = get_stock_info(ticker2)
 
+
+        #UX two columns
     col1, col2 = st.columns(2)
 
+
+        #column one 
     with col1:
         st.header(ticker1.upper())
-        st.write("Profit Margin:", stock1.get("profitMargins"))
+        st.subheader("Profit Margin:", stock1.get("profitMargins"))
         st.write("Revenue Growth:", stock1.get("revenueGrowth"))
         st.write("Return on Equity:", stock1.get("returnOnEquity"))
         st.write("P/E Ratio:", stock1.get("trailingPE"))
