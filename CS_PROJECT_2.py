c58e9b313ac713d5788cb67017d33f8a89ee3cce
Philippe Verdeja <philippeverdeja@gmail.com>
2026-03-17 21:18:50 +0100

    line 40

--- CS_PROJECT_2.py
+++ CS_PROJECT_2.py
@@ -37,8 +37,7 @@ def get_info(symbol: str) -> dict:
     price = t.price
     if isinstance(price, dict) and symbol in price and isinstance(price[symbol], dict):
         merged["longName"] = price[symbol].get("longName", symbol)
-    else:
-        merged ["longName"] = symbol
+    
 
     return merged
 
