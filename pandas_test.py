2b554151af00fa94646e7abbb23e888c72b1fd7b
Philippe Verdeja <philippeverdeja@gmail.com>
2026-03-29 13:51:10 +0200

    changes

--- pandas_test.py
+++ pandas_test.py
@@ -7,10 +7,20 @@ col1, col2, col3, col4 = st.columns (4)
 with col1:
     stock1 = st.text_input("first ticker")
 
+with col2:
+    stock2 = st.text_input("Second ticker")
+
+with col3:
+    stock3 = st.text_input("Third ticker")
+
+with col4:
+    stock4 = st.text_input("Fourth ticker")
 
 df = pd.DataFrame({
-    'Ticker': [stock1, 'MSFT'],
-    'Price': [150, 300]
+    stock1: ["price","pm", "rg", "ROE" ],
+    stock2: ["price2", "pm2", "rg2", "ROE2"]
+    stock3: ["price3", "pm3", "rg3", "ROE3"]
+    stock4: ["price4", "pm4", "rg4", "ROE4"]
 })
 
 st.dataframe(df)
