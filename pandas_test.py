322696697a859de0958691b67dfafa5a405e6026
Philippe Verdeja <philippeverdeja@gmail.com>
2026-03-29 13:47:18 +0200

    push

--- pandas_test.py
+++ pandas_test.py
@@ -2,8 +2,14 @@ import streamlit as st
 import pandas as pd
 from yahooquery import Ticker
 
+col1, col2, col3, col4 = st.columns (4)
+
+with col1:
+    stock1 = st.text_input("first ticker")
+
+
 df = pd.DataFrame({
-    'Ticker': [st.text_input("Insert Ticker"), 'MSFT'],
+    'Ticker': [stock1, 'MSFT'],
     'Price': [150, 300]
 })
 
