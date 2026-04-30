0116d7c3801b24d6f477376c823a89fd5b5b406c
Philippe Verdeja <philippeverdeja@gmail.com>
2026-04-30 14:39:38 +0200

    text effects

--- app/pages/5_Profile.py
+++ app/pages/5_Profile.py
@@ -339,7 +339,7 @@ if st.button("Show My Profile", type="primary", width="stretch"):    # This is t
 
 
         # This the displays th results.
-        st.title(f"Your Profile: {profile}")
+        st.title(f"Your Profile: {_profile_}")
         st.write(f"**Description:** {description}")
         st.divider()
         
