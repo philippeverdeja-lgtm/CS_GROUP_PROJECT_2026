6a96b38c6c895c3486a93d20cc47e1f174088923
Philippe Verdeja <philippeverdeja@gmail.com>
2026-04-30 13:41:03 +0200

    change buttons

--- app/Home.py
+++ app/Home.py
@@ -1,13 +1,21 @@
+#This is the Homepage of our Website. From here our users can navigate to 
+#all our other features by clicking on the buttons below the titles
+
+#The layout of the files in the folder creates also a navigator througout the entire website
+
+
 #this imports streamlit into the program
 import streamlit as st
 
-#page configuration wide
+#page configuration and titles
 st.set_page_config(layout="wide")
 
 st.title("Homepage Easy Investing")
 
 st.info("Welcome!")
 
+
+#This is a little monopoly man winking his eye
 st.markdown("""
     <style>
     .monopoly-man {
@@ -21,10 +29,21 @@ st.markdown("""
     <img class="monopoly-man" src="https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExbWtleW5nNnJqdjA1aW5hODRsZGhzZzE5ZTJpcHRydDR4ZDU0Z21qayZlcD12MV9zdGlja2Vyc19zZWFyY2gmY3Q9cw/C8976bDqhEUk40i8XU/giphy.gif">
 """, unsafe_allow_html=True)
 
+
+
+st.markdown("""
+    <style>
+    .stPageLink button {
+        background-color: #1f77b4 !important;
+        color: white !important;
+    }
+    </style>
+""", unsafe_allow_html=True)
+
 #divider for layout
 st.divider()
 
-# Layout with columns and themes
+# Layout with columns and themes for basic overview and understanding of our website
 col1, col2, col3, col4, col5 = st.columns(5) 
 
 with col1:
@@ -54,6 +73,9 @@ with col5:
 
 
 
+
+#Here are the buttons that allow the anvigation between the different pages of our website. 
+
 st.divider()
 
 col1, col2, col3, col4, col5 = st.columns(5)
@@ -71,9 +93,9 @@ with col4:
     st.page_link("pages/4_News.py", label="Go to News")
 
 with col5:
-    st.page_link("pages/1_Basics.py", label="Go to Profile")
+    st.page_link("pages/5_Profile.py", label="Go to Profile")
 
 
 st.divider()
 
-st.info("Use the sidebar to navigate between features")
+st.info("Use the sidebar or click on the buttons")
