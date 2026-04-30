36f8ee3dbb6412d897a52b3a8255c858f0a8d92c
Philippe Verdeja <philippeverdeja@gmail.com>
2026-04-30 13:30:54 +0200

    buttons at the bottom

--- app/Home.py
+++ app/Home.py
@@ -30,30 +30,49 @@ col1, col2, col3, col4, col5 = st.columns(5)
 with col1:
     st.subheader("Basics")
     st.write("Fundamentals of investing")
-    st.page_link("pages/1_Basics.py", label="Go to Basics")
+
 
 with col2:
     st.subheader("Analyzer")
     st.write("Create and analyze your portfolio")
-    st.page_link("pages/2_Analyzer.py", label="Go to Analyzer")
+
 
 with col3:
     st.subheader("Comparator")
     st.write("Compare stocks and ETFs")
-    st.page_link("pages/3_Comparator.py", label="Go to Comparator")
+
 
 with col4:
     st.subheader("News")
     st.write("Stay updated with latest information")
-    st.page_link("pages/4_News.py", label="Go to News")
+
 
 
 with col5:
     st.subheader("Profile")
     st.write("Find your investor profile!")
-    st.page_link("pages/1_Basics.py", label="Go to Profile")
+
 
 
 st.divider()
+
+col1, col2, col3, col4, col5 = st.columns(5)
+
+with col1:
+    st.page_link("pages/1_Basics.py", label="Go to Basics")
+
+with col2:
+    st.page_link("pages/2_Analyzer.py", label="Go to Analyzer")
+
+with col3:
+    st.page_link("pages/3_Comparator.py", label="Go to Comparator")
+
+with col4:
+    st.page_link("pages/4_News.py", label="Go to News")
+
+with col5:
+    st.page_link("pages/1_Basics.py", label="Go to Profile")
+
+    
 
 st.info("Use the sidebar to navigate between features")
